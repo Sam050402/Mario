c532b07cfc7760b0c6bbf700de1f8456ecf88a44
Sam050402 <106328859+Sam050402@users.noreply.github.com>
2023-03-28 12:15:30 +0530

    Create mario.js

---
+++ mario.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+	// Code to start the game
+}
